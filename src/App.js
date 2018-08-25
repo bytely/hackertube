64004a51eb206b415dfd26ca48434ebfb8c13bf7
Ebrahim <er8h@outlook.com>
2018-08-25 13:19:18 +0430

    refactor constants

--- src/App.js
+++ src/App.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
-import he from 'he';
-import * as _ from 'lodash';
+import React, { Component } from "react";
+import he from "he";
+import * as _ from "lodash";
 import "tabler-react/dist/Tabler.css";
-import { Page, Grid, Card, Container, Loader, Progress } from 'tabler-react/dist';
-import Waypoint from 'react-waypoint';
+import { Page, Grid, Card, Container } from "tabler-react/dist";
+import Waypoint from "react-waypoint";
+import { apiBasePath, youtubeLinkPattern } from "./constants";
 
-const baseUrl = 'https://api.hnpwa.com/v0'
+const getJson = _.partialRight(_.invoke, "json");
 
-const pattern = /\b(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9]{11})\b/gim
-
-const getJson = _.partialRight(_.invoke, 'json');
-
-const getHnUrl = (path) => `https://news.ycombinator.com/${path}`;
+const getHnUrl = path => `https://news.ycombinator.com/${path}`;
 
 export class App extends Component {
   state = {
     page: 1,
     loading: true,
-    items: [],
-  }
+    items: []
+  };
 
-  videoPosts = (item) =>
+  videoPosts = item =>
     _([item, ...item.comments.map(this.videoPosts)])
       .flatten()
       .map(it => ({
         ...it,
-        title: it.type === 'comment' ? item.title : it.title,
+        title: it.type === "comment" ? item.title : it.title,
         content: he.decode(it.content)
       }))
-      .filter(it => pattern.test(it.content))
+      .filter(it => youtubeLinkPattern.test(it.content))
       .flatMap(it =>
-        (it.content.match(pattern) || []).map(videoUrl => ({
+        (it.content.match(youtubeLinkPattern) || []).map(videoUrl => ({
           ...it,
-          videoId: videoUrl.match(new RegExp(pattern, 'i'))[1]
+          videoId: videoUrl.match(new RegExp(youtubeLinkPattern, "i"))[1]
         }))
       )
-      .uniqBy('videoId')
-      .orderBy('time')
-      .value()
+      .uniqBy("videoId")
+      .orderBy("time")
+      .value();
 
-  loadMore = () => 
-    !this.loading && this.loadPage(this.state.page + 1)
+  loadMore = () => !this.loading && this.loadPage(this.state.page + 1);
 
-  loadPage = (page) => {
+  loadPage = page => {
     this.setState({
       loading: true
     });
 
-    fetch(`${baseUrl}/news/${this.state.page}.json`)
+    fetch(`${apiBasePath}/news/${this.state.page}.json`)
       .then(getJson)
       .then((fetchedItems = []) => {
         fetchedItems.forEach(async item => {
@@ -57,63 +53,67 @@ export class App extends Component {
               items: [...items, ...videoItems]
             }));
           }
-        })
+        });
       })
-      .then(() => this.setState(({ page }) => ({
-        page: Math.min(page + 1, 10)
-      })))
-      .finally(() => this.setState({
-        loading: false
-      }));
-  }
+      .then(() =>
+        this.setState(({ page }) => ({
+          page: Math.min(page + 1, 10)
+        }))
+      )
+      .finally(() =>
+        this.setState({
+          loading: false
+        })
+      );
+  };
 
   fetchItem({ id }) {
-    return fetch(`${baseUrl}/item/${id}.json`)
+    return fetch(`${apiBasePath}/item/${id}.json`)
       .then(getJson)
-      .then(this.videoPosts)
+      .then(this.videoPosts);
   }
 
   componentDidMount() {
-    this.loadMore()
+    this.loadMore();
   }
 
   render() {
     return (
       <Page>
-      <Page.Main>
-      <Container className='text-center my-4'>
-        <h1>HackerTube</h1>
-      </Container>
-      <Container>
-      {
-        this.state.items.map(item => (
-          <Grid.Row cards key={item.videoId}>
-          <Grid.Col md={8} offsetMd={2} OffsetSm={0}>
-          <Card title={<a target="_blank" href={getHnUrl(item.url)}>{item.title}</a>}>
-            <Card.Body style={{padding: 0}}>
-            <iframe
-              type='text/html'
-              src={`https://youtube.com/embed/${item.videoId}`}
-              title={item.videoId}
-              width='100%'
-              height='320'
-              frameBorder='0'
-              allowFullScreen={true}
-            >
-            </iframe>
-            </Card.Body>
-          </Card>
-          </Grid.Col>
-          </Grid.Row>
-        ))
-      }
-      <Waypoint
-        bottomOffset={-80}
-        onEnter={this.loadMore} />
-      </Container>
-      </Page.Main>
+        <Page.Main>
+          <Container className="text-center my-4">
+            <h1>HackerTube</h1>
+          </Container>
+          <Container>
+            {this.state.items.map(item => (
+              <Grid.Row cards key={item.videoId}>
+                <Grid.Col md={8} offsetMd={2} OffsetSm={0}>
+                  <Card
+                    title={
+                      <a target="_blank" href={getHnUrl(item.url)}>
+                        {item.title}
+                      </a>
+                    }
+                  >
+                    <Card.Body style={{ padding: 0 }}>
+                      <iframe
+                        type="text/html"
+                        src={`https://youtube.com/embed/${item.videoId}`}
+                        title={item.videoId}
+                        width="100%"
+                        height="320"
+                        frameBorder="0"
+                        allowFullScreen={true}
+                      />
+                    </Card.Body>
+                  </Card>
+                </Grid.Col>
+              </Grid.Row>
+            ))}
+            <Waypoint bottomOffset={-80} onEnter={this.loadMore} />
+          </Container>
+        </Page.Main>
       </Page>
     );
   }
 }
-
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const apiBasePath = "https://api.hnpwa.com/v0";
+
+export const youtubeLinkPattern = /\b(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9]+)\b/gim;
