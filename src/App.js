48521bd4a4aef89020ea588b6d1bd697ea482c2f
Ebrahim <er8h@outlook.com>
2018-08-25 13:09:59 +0430

    add 'load more'

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import he from 'he';
 import * as _ from 'lodash';
 import "tabler-react/dist/Tabler.css";
-import { Page, Grid, Card, Container } from 'tabler-react/dist';
+import { Page, Grid, Card, Container, Loader, Progress } from 'tabler-react/dist';
+import Waypoint from 'react-waypoint';
 
-const newsUrl = 'https://api.hnpwa.com/v0/news'
-const itemUrl = 'https://api.hnpwa.com/v0/item'
+const baseUrl = 'https://api.hnpwa.com/v0'
 
 const pattern = /\b(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9]{11})\b/gim
 
+const getJson = _.partialRight(_.invoke, 'json');
+
+const getHnUrl = (path) => `https://news.ycombinator.com/${path}`;
 
 export class App extends Component {
   state = {
-    page: 7,
+    page: 1,
+    loading: true,
     items: [],
   }
 
@@ -35,15 +39,16 @@ export class App extends Component {
       .orderBy('time')
       .value()
 
-  fetchItem({ id }) {
-    return fetch(`${itemUrl}/${id}.json`)
-      .then(res => res.json())
-      .then(this.videoPosts)
-  }
+  loadMore = () => 
+    !this.loading && this.loadPage(this.state.page + 1)
 
-  componentDidMount() {
-    fetch(`${newsUrl}/${this.state.page}.json`)
-      .then(res => res.json())
+  loadPage = (page) => {
+    this.setState({
+      loading: true
+    });
+
+    fetch(`${baseUrl}/news/${this.state.page}.json`)
+      .then(getJson)
       .then((fetchedItems = []) => {
         fetchedItems.forEach(async item => {
           const videoItems = await this.fetchItem(item);
@@ -54,23 +59,38 @@ export class App extends Component {
           }
         })
       })
+      .then(() => this.setState(({ page }) => ({
+        page: Math.min(page + 1, 10)
+      })))
+      .finally(() => this.setState({
+        loading: false
+      }));
+  }
+
+  fetchItem({ id }) {
+    return fetch(`${baseUrl}/item/${id}.json`)
+      .then(getJson)
+      .then(this.videoPosts)
+  }
+
+  componentDidMount() {
+    this.loadMore()
   }
 
   render() {
     return (
       <Page>
-      <Page.Title>HackerTube</Page.Title>
       <Page.Main>
+      <Container className='text-center my-4'>
+        <h1>HackerTube</h1>
+      </Container>
       <Container>
       {
         this.state.items.map(item => (
-          <Grid.Row cards gutters='md'>
-          <Grid.Col width={6}>
-          <Card
-            key={item.videoId}
-            title={item.title}
-          >
-            <Card.Body>
+          <Grid.Row cards key={item.videoId}>
+          <Grid.Col md={8} offsetMd={2} OffsetSm={0}>
+          <Card title={<a target="_blank" href={getHnUrl(item.url)}>{item.title}</a>}>
+            <Card.Body style={{padding: 0}}>
             <iframe
               type='text/html'
               src={`https://youtube.com/embed/${item.videoId}`}
@@ -78,7 +98,7 @@ export class App extends Component {
               width='100%'
               height='320'
               frameBorder='0'
-              allowfullscreen='allowfullscreen'
+              allowFullScreen={true}
             >
             </iframe>
             </Card.Body>
@@ -87,6 +107,9 @@ export class App extends Component {
           </Grid.Row>
         ))
       }
+      <Waypoint
+        bottomOffset={-80}
+        onEnter={this.loadMore} />
       </Container>
       </Page.Main>
       </Page>
